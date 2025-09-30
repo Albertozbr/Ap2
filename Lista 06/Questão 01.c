9642de6a5406c0d448df6fe642fd2d8d2e8f9a92
Alberto <albertoentreportes11@gmail.com>
2025-09-30 19:47:15 -0300

    Create Questão 01.c

---
+++ Lista 06/Questão 01.c
@@ -0,0 +1,27 @@
+#QUESTÃO 01
+##a)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int troca(int x, int y){
+    int aux;
+
+    printf("%d %d\n", a, b);
+
+    c = a;
+    a = b;
+    b = c;
+
+    printf("%d %d", a, b);
+}
+
+void main(){
+
+    int a, b, aux;
+    printf("Informe dois numeros 'a' e 'b': \n");
+    scanf("%d %d", &a, &b);
+
+    troca(aux);
+}
